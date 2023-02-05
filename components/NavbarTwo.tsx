e5478963caf70d31a621fc56a9ed242c38f04c0d
Joseph Charles <112246775+jccdevweb@users.noreply.github.com>
2023-02-05 20:33:23 +0800

    Implemented Hamburger Close when Link is Clicked

--- components/NavbarTwo.tsx
+++ components/NavbarTwo.tsx
@@ -13,7 +13,9 @@ const NavbarTwo = () => {
     {name:'CONTACT', link:'/contact',id:'7',},
   ]
   const [showNav, setShownav] = useState(false);
-
+  const HandleHamburger = () => {
+    setShownav(!showNav)
+  }
   console.log(showNav)
   return (
     <div className='shadow-md w-full fixed top-0 left-0 z-10'>
@@ -23,19 +25,19 @@ const NavbarTwo = () => {
           <Link className="cursor-pointer" href = "/" >JCC</Link>
           </span>
         </div>
-        <div onClick={()=>setShownav(!showNav)}className="text-black text-3xl absolute right-8 top-6 cursor-point md:hidden cursor-pointer">
+        <div onClick={HandleHamburger} className="text-black text-3xl absolute right-8 top-6 cursor-point md:hidden cursor-pointer">
         <svg xmlns="http://www.w3.org/2000/svg"   fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d={showNav ? 'M6 18L18 6M6 6l12 12':'M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'} />
         </svg>
         </div>
-        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0  transition-all duration-500 ease-in ${showNav ? 'top-15' : 'top-[-490px]'}`
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0  transition-all duration-500 ease-in ${showNav ? 'top-15opacity-100' : 'top-[-490px] opacity-0 md:opacity-100'}`
       }>
        
         {
           
           Links.map((link)=>(
             <li key={link.id} className="nav-item md:ml-8 text-xl md:my-0 my-7">
-              <Link className="nav-item text-gray-800 hover:text-gray-400 duration-500" href={link.link}>{link.name}</Link>
+              <Link onClick={HandleHamburger} className="nav-item text-gray-800 hover:text-gray-400 duration-500" href={link.link}>{link.name}</Link>
             </li>))
         }
         </ul>
